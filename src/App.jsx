65c9b2e8a73a8222aa7ebf315848266b7b71972e
dokens <robinlefevre85@gmail.com>
2024-08-04 21:27:58 +0200

    Initialisation du projet, ajout Navbar et section home

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.sass';
+
+import Home from './components/Home/Home.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
+
+function App() {
+    return (
+        <div className='App'>
+            <Navbar />
+            <Home />
+        </div>
+    );
+}
+
+export default App;
+
